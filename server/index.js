d350e96763b50387f5129f2a5b5b249fdcf492a1
deHypeChief <dev.hype7@gmail.com>
2024-01-25 12:45:39 +0100

    ui update

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const connectToMongoDB = () => {
 connectToMongoDB();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
 
